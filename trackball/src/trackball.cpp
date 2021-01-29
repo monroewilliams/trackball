d3436c41151c6b26d410857085d19edc13fd01b4
Monroe Williams <monroe@pobox.com>
2021-01-28 17:44:27 -0800

    Rename main .ino file to .cpp, simplify include directives.

--- trackball/src/trackball.cpp
+++ trackball/src/trackball.cpp
@@ -1,11 +1,7 @@
 #include <SPI.h>
 #include <avr/pgmspace.h>
 
-#if ARDUINO >= 10606
-  // If we have support for pluggable HID/USB modules, make this device look like just a mouse.
-  #include <HID.h>
-  #include <Mouse.h>
-#endif
+#include <Mouse.h>
 
 #include "trackball.h"
 
